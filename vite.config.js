78eb8bf131d6566684f3f77b888d5b776d1d6684
Wisse <wisse@whomadethis.nl>
2026-04-15 11:00:24 +0200

    Fix process.env not defined in IIFE build

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
       ],
     }),
   ],
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  },
   build: {
     lib: {
       entry: 'src/main.jsx',
